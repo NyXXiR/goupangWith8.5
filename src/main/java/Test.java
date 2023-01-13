206803097249382225cd6901f28cd05aade13f7a
NyXXiR <lgh0334@gmail.com>
2023-01-13 15:25:25 +0900

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,9 +2,6 @@ import java.util.Arrays;
 import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.jasper.tagplugins.jstl.core.ForEach;
-
-import model.itemVO;
 import mybatis.Mybatis;
 
 public class Test {
@@ -15,42 +12,42 @@ public class Test {
     SqlSessionFactory sqlSessionFactory = Mybatis.getSqlSessionFactory();
     SqlSession sess;
     sess = sqlSessionFactory.openSession(true);
- 
-//     itemVO vo = new itemVO(0, "banana", 3000, 0, "lgh0334", "sysdate", "toy");
-//     int n = session.insert("add", vo);
-//     if (n > 0) {
-//     session.commit();
-//     System.out.println("추가 성공");
-//     } else {
-//     session.rollback();
-//     System.out.println("추가 실패");
-//    
-//     }
-    dafdsgsgsdgs
-     String a = sess.selectOne("selectName", 3);
 
-//     String b = session.selectOne("selectPrice", 4);
-//     System.out.println(a);
-//
-//     List<itemVO> list = session.selectList("selectByPrice");
-//     System.out.println(list);
+    // itemVO vo = new itemVO(0, "banana", 3000, 0, "lgh0334", "sysdate", "toy");
+    // int n = session.insert("add", vo);
+    // if (n > 0) {
+    // session.commit();
+    // System.out.println("추가 성공");
+    // } else {
+    // session.rollback();
+    // System.out.println("추가 실패");
+    //
+    // }
 
-     int itemRecord = sess.selectOne("ItemSaleRecordSum");
-		List<Integer> salList = sess.selectList("ItemSaleRecordSum");
-		
-		salList.stream().flatMapToDouble( t ->	{
-			Integer str = Integer.valueOf(t) ;
-			double dob = (double)str;
-			dob /= itemRecord;
-			double[] doblist = null;
-			return Arrays.stream(doblist);
-			}).forEach(t -> System.out.println(t));
-		
-     
+    String a = sess.selectOne("selectName", 3);
+
+    // String b = session.selectOne("selectPrice", 4);
+    // System.out.println(a);
+    //
+    // List<itemVO> list = session.selectList("selectByPrice");
+    // System.out.println(list);
+
+    int itemRecord = sess.selectOne("ItemSaleRecordSum");
+    List<Integer> salList = sess.selectList("ItemSaleRecordSum");
+
+    salList.stream().flatMapToDouble(t -> {
+      Integer str = Integer.valueOf(t);
+      double dob = (double) str;
+      dob /= itemRecord;
+      double[] doblist = null;
+      return Arrays.stream(doblist);
+    }).forEach(t -> System.out.println(t));
+
+
     // int n= session.insert("add", vo);
-     
-		
-	
+
+
+
   }
 
 }
